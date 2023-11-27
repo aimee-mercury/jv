4661847d8dbcfe51e8693685cbf403b431eff4aa
marie <ingabiremarieaimee6@gmail.com>
2023-11-27 11:37:45 -0800

    my push

---
+++ day3.js
@@ -0,0 +1,21 @@
+ let  height  =  180;
+ let  anotherHeight  =  height;
+ let  weight;
+ console.log(height); 
+ console.log(anotherHeight);  
+ weight  =  70;    
+ console.log(weight); 
+
+ "use  strict";
+ let remy  = 20;
+ console.log(remy);  
+ console.log("remy"); 
+ 
+ let steps =120;
+ console.log(steps);
+ steps=150;
+ console.log(steps);
+ steps=steps+200;
+ console.log(steps);
+
+ 
